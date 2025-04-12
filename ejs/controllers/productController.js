be2992d5223c37ef6f8ec4c1d7eb10f210866233
Teodelina Lacroze <teodelina@MacBook-Air-de-Teodelina.local>
2025-04-12 20:06:51 -0300

    arregle la ruta de profile

--- ejs/controllers/productController.js
+++ ejs/controllers/productController.js
@@ -1,9 +1,13 @@
+const database = require('../db/database')
+
 const productController= {
     product: function(req, res) {
         res.render('product');
     },
     productadd: function(req, res) {
-        res.render('productadd', {title: 'Product add'});
+        res.render('productadd', {
+            usuario: database.usuario
+        });
     }
 }
 
--- ejs/routes/profile.js
+++ ejs/routes/profile.js
@@ -1,7 +1,10 @@
 const express = require('express');
 var router = express.Router();
 const profileController = require('../controllers/headerLogController');
+const profileController2 = require('../controllers/profileController')
 
 router.get('/', profileController.profile);
+router.get('/', profileController2.profile);
+
 
 module.exports = router;
