334a3b92540021b9ce80ce8def1e202cf3e65e44
franciscogoni <gonif@github.com>
2025-04-14 11:46:11 -0300

    Cambios a index.ejs

--- ejs/db/database.js
+++ ejs/db/database.js
@@ -16,27 +16,157 @@ const data = {
             imagen: "ejs/public/images/products/img-tv-samsung-smart.jpg",
             comentarios: [
                 {
-                    nombre: "Juan",
+                    nombre: "juanHierro",
                     comentario: "Me gusto mucho.",
                     imagen: "ejs/public/images/fotodePerfil.jpg"
                 },
                 {
-                    nombre: "Ana",
+                    nombre: "ana123",
                     comentario: "Muy utíl.",
                     imagen: "/images/fotodePerfil.jpg"
                 },
                 {
-                    nombre: "Luisa",
+                    nombre: "luisa_ok",
                     comentario: "Tan buena.",
                     imagen: "/images/fotodePerfil.jpg"
-                },
-
+                }
             ]
         },
-    
-    ]
-        
+        {
+            nombre: "Irobot Roomba",
+            descripcion: "Aspirador automata",
+            imagen: "ejs/public/images/products/02dee987-233b-4e0c-8464-b2bc86483fe7.jpg",
 
+            comentarios: [
+                {
+                    nombre: "sofiii",
+                    comentario: "Ni un ruido.",
+                   imagen: "ejs/public/images/fotodePerfil.jpg"
+                },
+                {
+                    nombre: "markus",
+                    comentario: "Limpio mi cuarto en 2 minutos.",
+                    imagen: "/images/fotodePerfil.jpg"
+                },
+                {
+                    nombre: "ana123",
+                    comentario: "A mi perros no les gusta :(",
+                    imagen: "/images/fotodePerfil.jpg"
+                }
+            ]
+        },
+
+        {
+            nombre: "Magsafe charger",
+            descripcion: "Cargador inalambrico",
+            imagen: "ejs/public/images/products/6e13385a-2443-47de-985d-b86fb8063f7b.jpg",
+            comentarios: [
+                {
+                    nombre: "juanHierro",
+                    comentario: "No mas cables rotos.",
+                    imagen: "ejs/public/images/fotodePerfil.jpg"
+                },
+                {
+                    nombre: "markus",
+                    comentario: "Eficiente.",
+                    imagen: "/images/fotodePerfil.jpg"
+                },
+                {
+                    nombre: "luisa_ok",
+                    comentario: "Facil de usar.",
+                    imagen: "/images/fotodePerfil.jpg"
+                },
+            ]
+        },
+        {
+            nombre: "Mouse argonomico",
+            descripcion: "Mouse para largas sesiones en la computadora con la mejor comodidad.",
+            imagen: "ejs/public/images/products/76e8e952-9a85-4db6-826a-f6f6a604891c.jpg",
+            comentarios: [
+                {
+                    nombre: "sofiii",
+                    comentario: "Con este mouse mi mano toco el cielo, muy comodo.",
+                    imagen: "ejs/public/images/fotodePerfil.jpg"
+                },
+                {
+                    nombre: "markus",
+                    comentario: "Muy bueno para mi trabajo como diseñador.",
+                    imagen: "/images/fotodePerfil.jpg"
+                },
+                {
+                    nombre: "ana123",
+                    comentario: "Lo use como por 3 horas seguidas y no senti incomada mi mano 10/10.",
+                    imagen: "/images/fotodePerfil.jpg"
+                },
+            ]
+        },
+        {
+            nombre: "Airpods 3 generación",
+            descripcion: "Auriculares de inalambricos de Apple.",
+            imagen: "ejs/public/images/products/6e13385a-2443-47de-985d-b86fb8063f7b.jpg",
+            comentarios: [
+                {
+                    nombre: "sofiii",
+                    comentario: "En el tren, bondi y en cualquier lugar, son infaltables.",
+                    imagen: "ejs/public/images/fotodePerfil.jpg"
+                },
+                {
+                    nombre: "markus",
+                    comentario: "Aguante Cerati, estos auris le hacen justicia.",
+                    imagen: "/images/fotodePerfil.jpg"
+                },
+                {
+                    nombre: "luisa_ok",
+                    comentario: "Los bajos suenan increible",
+                    imagen: "/images/fotodePerfil.jpg"
+                },
+            ]
+        },
+        {
+            nombre: "Macbook Airpro",
+            descripcion: "Laptop carismatica de Apple.",
+            imagen: "ejs/public/images/products/6e13385a-2443-47de-985d-b86fb8063f7b.jpg",
+            comentarios: [
+                {
+                    nombre: "ana123",
+                    comentario: "Muy bueno el procesamiento.",
+                    imagen: "ejs/public/images/fotodePerfil.jpg"
+                },
+                {
+                    nombre: "sofiii",
+                    comentario: "Apple > cauqluier compu.",
+                    imagen: "/images/fotodePerfil.jpg"
+                },
+                {
+                    nombre: "juanHierro",
+                    comentario: "Nunca mas quiero otra computadora.",
+                    imagen: "/images/fotodePerfil.jpg"
+                },
+            ]
+        },
+        {
+            nombre: "Samsung galaky",
+            descripcion: "Cargador inalambrico",
+            imagen: "ejs/public/images/products/6e13385a-2443-47de-985d-b86fb8063f7b.jpg",
+            comentarios: [
+                {
+                    nombre: "ana123",
+                    comentario: "No mas cables rotos.",
+                    imagen: "ejs/public/images/fotodePerfil.jpg"
+                },
+                {
+                    nombre: "juanHierro",
+                    comentario: "Eficiente.",
+                    imagen: "/images/fotodePerfil.jpg"
+                },
+                {
+                    nombre: "markus",
+                    comentario: "Facil de usar.",
+                    imagen: "/images/fotodePerfil.jpg"
+                },
+            ]
+        },
+    ]
 }
 
 module.exports = data;
