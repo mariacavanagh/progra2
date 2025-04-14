0c2b706c545dbe2f820d728832d31273393fa2fc
Teodelina Lacroze <teodelina@MacBook-Air-de-Teodelina.local>
2025-04-14 17:26:27 -0300

    logramos que ande search

--- ejs/controllers/productController.js
+++ ejs/controllers/productController.js
@@ -3,7 +3,11 @@ const database = require('../db/database');
 const productController= {
     product: function(req, res) {
         const miProducto = database.productos[0];
-        res.render('product' , {productos: miProducto});
+
+        res.render('product' , {
+            product: miProducto,
+            usuario: database.usuario
+        });
          
     },
     productadd: function(req, res) {
--- ejs/controllers/searchresultsController.js
+++ ejs/controllers/searchresultsController.js
@@ -2,6 +2,8 @@ const data = require("../db/database");
 
 const searchresultsController= {
     searchresults: function(req, res) {
+        const resultado = data.productos[0];
+
         res.render('search-result', {
             title:"Resultados de busqueda",
             productos: data.productos
