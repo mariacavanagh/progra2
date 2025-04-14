99f340505abb4ecc4d6cb0444cd62370235d7f9b
franciscogoni <gonif@github.com>
2025-04-14 09:05:14 -0300

    mas cheque con lucas

--- ejs/db/database.js
+++ ejs/db/database.js
@@ -2,7 +2,6 @@
 const data = {
     usuario: [
     {
-        id: 1,
         email: "juan05@gmail.com",
         usuario: "Juan Antonio Hierro",
         contraseña:"Perr@123",
@@ -12,17 +11,19 @@ const data = {
     ],
 
     productos: [
-        { id: 1, usuario_id: 1, nombreArchivo: 'producto1.jpg', nombreProducto: '', descripcionProducto: '' },
-        { id: 2, usuario_id: 1, nombreArchivo: 'producto2.jpg', nombreProducto: '', descripcionProducto: '' },
-        { id: 3, usuario_id: 2, nombreArchivo: 'producto3.jpg', nombreProducto: '', descripcionProducto: '' },
-        { id: 4, usuario_id: 2, nombreArchivo: 'producto4.jpg', nombreProducto: '', descripcionProducto: '' },
-        { id: 5, usuario_id: 3, nombreArchivo: 'producto5.jpg', nombreProducto: '', descripcionProducto: '' },
-        { id: 6, usuario_id: 3, nombreArchivo: 'producto6.jpg', nombreProducto: '', descripcionProducto: 'Estilo retro para decoración' },
-        { id: 7, usuario_id: 4, nombreArchivo: 'producto7.jpg', nombreProducto: '', descripcionProducto: '' },
-        { id: 8, usuario_id: 4, nombreArchivo: 'producto8.jpg', nombreProducto: '', descripcionProducto: '' },
-        { id: 9, usuario_id: 5, nombreArchivo: 'producto9.jpg', nombreProducto: '', descripcionProducto: 's' },
-        { id: 10, usuario_id: 5, nombreArchivo: 'producto10.jpg', nombreProducto: 'Auriculares Bluetooth', descripcionProducto: 'Con cancelación de ruido' }
-      ]
+        {
+            nombre: "tele",
+            descripcion: "es muy buena",
+            imagen: "/images/tele.png",
+            comentarios: [
+                {
+                    nombre: "juan",
+                    comentario: "me gusto mucho",
+                    imagen: "/imagenes/users/usuario1.png"
+                }
+            ]
+        },
+    ]
 
 }
 
