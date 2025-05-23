be2e54dfe42c986fad00f136d90be47c27a9c4a0
Maria <mcavanaghnicholson@udesa.edu.ar>
2025-05-23 17:05:58 -0300

    hicimos el primer punto, nos falta el final nada mas

--- ejs/app.js
+++ ejs/app.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/index', mainRouter);
+app.use('/', mainRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/headerlog', headerlogRouter);
