a8d62cb1014a263b24dc6c89a6d7e404abe54080
franciscogoni <gonif@github.com>
2025-05-26 11:53:16 -0300

    terminando punto 2

--- ejs/controllers/registerController.js
+++ ejs/controllers/registerController.js
@@ -1,40 +1,49 @@
-const data = require ("../db/database")
-const db = require("../database/models")
-const bycrypt = require(".")
+const data = require("../db/database");
+const db = require("../database/models");
+const bcrypt = require("bcryptjs");
 
-const registerController= {
+const registerController = {
     register: function(req, res) {
-        res.render('register', {title: 'Register'});
+        res.render('register', { title: 'Register' });
+    },
+
+    proccessRegister: function(req, res) {
+        const usuario = req.body.usuario;
+        const email = req.body.email;
+        const contrasena = req.body.contrasena;
+        const fechaNacimiento = req.body.fechaNacimiento;
+
+        if (!email) {
+            return res.render("register", { error: "El email no puede estar vacío." });
+        }
+
+        if (!contrasena) {
+            return res.render("register", { error: "La contraseña no puede estar vacía." });
+        }
+
+        if (contrasena.length < 3) {
+            return res.render("register", { error: "La contraseña debe tener al menos 3 caracteres." });
+        }
+
+        db.User.findOne({ where: { email: email } })
+        .then(function(resultado) {
+            if (resultado) {
+                return res.render("register", { error: "Ese email ya está registrado." });
+            }
+
+            const contrasenaEncriptada = bcrypt.hashSync(contrasena, 10);
+
+            db.User.create({
+                usuario: usuario,
+                email: email,
+                contrasena: contrasenaEncriptada,
+                fechaNacimiento: fechaNacimiento
+            })
+            .then(function() {
+                return res.redirect("/login");
+            });
+        });
     }
-}
+};
 
-proccessRegister: function (req,res){
-    const usuario = req.body.usuario;
-    const email = req.body.email;
-    const contrasena = req.body.contrasena;
-    const fechaNacimiento = req.body.fechaNacimiento;
-
-    if (!email) {
-        return res.render("register", {error: "Esta seccion no puede estar vacia."})
-    }
-
-    if(!password) {
-        return res.render("register", {error: "Esta seccion no puede estar vacia."})
-    }
-
-    if (password.length <3 ){
-        return res.render("register", {error: "La contraseña debe tener por lo menos 3 caracteres."})
-    }
-
-    const contrasenaEncriptada = bycrypt.hashSync(contrasena, 10);
-
-    db.User.create({
-        usuario: usuario,
-        email: email,
-        contrasena: contrasenaEncriptada,
-        fechaNacimiento: fechaNacimiento
-    }).then(function{}){
-
-    }
-}
 module.exports = registerController;
