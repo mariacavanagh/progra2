520a8a91763e30592d6417e6620ebe88d09c617b
Teodelina Lacroze <teodelina@MacBook-Air-de-Teodelina.local>
2025-05-26 12:25:42 -0300

    search

--- ejs/controllers/searchresultsController.js
+++ ejs/controllers/searchresultsController.js
@@ -1,13 +1,23 @@
 const data = require("../db/database");
+const db = require('../database/models');
+const Sequelize = require("sequelize");
+const Op        = Sequelize.Op;
 
 const searchresultsController= {
     searchresults: function(req, res) {
-        const resultado = data.productos[0];
-
-        res.render('search-result', {
-            title:"Resultados de busqueda",
-            productos: data.productos
-        });
+        const buscar = req.query.search;
+        db.Product.findAll({
+            where: {
+                title:[{[Op.like]: "%{search}%"}]
+            }
+        }).then(function(resultados){
+            return res.render('search-result', {
+                title:"Resultados de busqueda",
+                productos: data.productos
+            });
+        }).catch(function(error){
+            return res.send(error);
+        })
     }
 }
 
